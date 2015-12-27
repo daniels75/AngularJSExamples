a158f58a856c095929d8081cb2b76e7714264dc0
Daniel Sadowski <daniel.sadowski75@gmail.com>
2015-12-27 20:50:53 +0100

    angularjs-sharing-data-between-controllers

---
+++ eggHeadExamples/app/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module("myApp", ["ui.router"])
+    .config(function config($stateProvider) {
+        $stateProvider.state("index", {
+            url: "",
+            controller: "FirstController as first",
+            templateUrl: "templates/first.html"
+        })
+        $stateProvider.state("second", {
+            url: "/second",
+            controller: "SecondController as second",
+            templateUrl: "templates/second.html"
+        })
+
+    })
+
+    .service('greeting', function Greeting() {
+        var greeting = this;
+        greeting.message = "Default";
+    })
+    .controller('FirstController', function(greeting) {
+        var first = this;
+        first.greeting = greeting;
+    })
+    .controller('SecondController', function(greeting) {
+        var second = this;
+        second.greeting = greeting;
+    })
+;
+
