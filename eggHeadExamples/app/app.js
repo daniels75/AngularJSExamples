090b2cb01f1725d7700ec0ab5e36a97d2b222e4f
Daniel Sadowski <daniel.sadowski75@gmail.com>
2015-12-30 20:58:26 +0100

    Added controller + directive

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -1,23 +1,23 @@
 'use strict';
 
 var app = angular.module("myApp", []);
-app.controller("AppCtrl", function($scope){
-    $scope.loadMoreTweets = function(){
-        alert("Load more tweets");
-    }
-    $scope.deleteMoreTweets = function(){
-        alert("Delete more tweets");
-    }
-});
-app.directive("enter", function(){
-    return function(scope, element, attrs) {
-        element.bind("mouseenter", function(){
-           // 1-st scope.loadMoreTweets();
-            // 2nd scope.$apply("loadMoreTweets()")
-            scope.$apply(attrs.enter)
-        });
-    }
 
+app.controller("ChoreCtr", function($scope) {
+    $scope.logChore = function(chore){
+        console.log(chore + "is done" )
+    }
+})
+
+app.directive("kid", function(){
+    return {
+        restrict: "E",
+        scope: {
+            done: "&"
+        },
+        template: '<input type="text" ng-model="chore">' +
+        ' {{chore}} </br> ' +
+        '<button ng-click="done({chore:chore})">I\'m done</button>'
+    }
 });
 
 
