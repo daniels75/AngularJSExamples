53be494172ad8b7aa14be01ef5a189c132e0f951
Daniel Sadowski <daniel.sadowski75@gmail.com>
2015-12-31 17:24:21 +0100

    Modified

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -2,25 +2,28 @@
 
 var app = angular.module("myApp", []);
 
-app.controller("ChoreCtr", function($scope) {
-    $scope.logChore = function(chore){
-        console.log(chore + "is done" )
+app.controller("AppCtrol", function($scope) {
+    $scope.ctrlFlavor = 'blackberry';
+})
+// 2nd approach
+app.directive("drink", function(){
+    return {
+        scope: {
+            flavor: '@'
+        },
+        template: "<div>{{flavor}}</div>"
     }
 })
-
-app.directive("kid", function(){
+/* 1st approach
+app.directive("drink", function(){
     return {
-        restrict: "E",
-        scope: {
-            done: "&"
-        },
-        template: '<input type="text" ng-model="chore">' +
-        ' {{chore}} </br> ' +
-        '<button ng-click="done({chore:chore})">I\'m done</button>'
+        template: "<div>{{flavor}}</div>",
+        link: function(scope, element, attrs) {
+            scope.flavor =  attrs.flavor
+        }
     }
-});
-
-
+})
+*/
 
 
 
