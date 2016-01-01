6e212672abf8ab7326a6b08cad391dc22ca70c73
Daniel Sadowski <daniel.sadowski75@gmail.com>
2016-01-01 15:16:31 +0100

    modified

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -1,15 +1,27 @@
 'use strict';
-
 var app = angular.module("myApp", []);
+var phoneApps = {};
 
-app.controller("AppCtrl", function($scope){
+phoneApps.controllers = {};
+phoneApps.controllers.AppCtrl = function($scope) {
 
     this.sayHi = function() {
         console.log("Say Hi");
     }
 
-     return $scope.AppCtrl = this;
-})
+    return $scope.AppCtrl = this;
+}
+
+phoneApps.directives = {};
+phoneApps.directives.panel = function(){
+    return {
+        restrict: 'E'
+    }
+}
+
+app.controller(phoneApps.controllers);
+app.directive(phoneApps.directives);
+
 
 
 
