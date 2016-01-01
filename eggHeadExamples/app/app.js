80e3acd62d68033dec92e8d0393a228df9490219
Daniel Sadowski <daniel.sadowski75@gmail.com>
2016-01-01 15:00:53 +0100

    modified

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -3,18 +3,13 @@
 var app = angular.module("myApp", []);
 
 app.controller("AppCtrl", function($scope){
-    $scope.callHome = function(message) {
-        console.log("calling home: " + message);
+
+    this.sayHi = function() {
+        console.log("Say Hi");
     }
 
+     return $scope.AppCtrl = this;
 })
 
-app.directive("panel", function(){
-    return {
-        restrict: 'E',
-        transclude: true,
-        template: '<div ng-transclude class="callout">This is a panel component</div>'
-    }
-});
 
 
