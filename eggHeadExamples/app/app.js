889d26e829cbe662c7d877bd4d76d0d3a4d20196
Daniels <daniel.sadowski75@gmail.com>
2016-01-26 20:54:49 +0100

    init angularjs-scope-vs-scope - part1

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -1,24 +1,15 @@
 'use strict';
 var app = angular.module("myApp", []);
-var validElement = angular.element("    <div>{{model.input}}</div>");
-this.link = function(scope, element) {
-    scope.$watch("model.input", function (value) {
-        if (value === "password") {
-            console.log("change it");
-            validElement.toggleClass("callout alert");
-        }
 
-    });
-};
-app.directive("dumbPassword", function(){
+app.controller("MyCtrl", function($scope) {
+    console.log($scope);
+});
+app.directive("myDirective", function(){
     return {
         restrict: 'E',
-        compile: function(tElem){
-            tElem.append(validElement);
-            return link;
-        },
-        replace: true,
-        template: '<div>\n    <input type="text" ng-model="model.input">\n</div>'
+        link: function(scope) {
+            console.log(scope);
+        }
     }
 })
 
