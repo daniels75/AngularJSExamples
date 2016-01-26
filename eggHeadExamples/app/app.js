df7d1f3f2f59fa9770ec11da1e2230f468a8c6b2
Daniels <daniel.sadowski75@gmail.com>
2016-01-26 21:20:40 +0100

    init angularjs-scope-vs-scope - part2

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -4,11 +4,16 @@ var app = angular.module("myApp", []);
 app.controller("MyCtrl", function($scope) {
     console.log($scope);
 });
+app.controller("MyCtrl2", ['$scope', '$http', function(a, b) {
+    console.log(b);
+}]
+);
 app.directive("myDirective", function(){
     return {
+        scope: {},
         restrict: 'E',
-        link: function(scope) {
-            console.log(scope);
+        link: function(scope, element, attrs) {
+            console.log(element);
         }
     }
 })
