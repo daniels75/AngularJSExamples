4ec88a196873ee7fc2d5c241767bdc956c5ca3f7
Daniels <daniel.sadowski75@gmail.com>
2016-01-26 20:39:42 +0100

    fixed linked function

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -2,19 +2,45 @@
 var app = angular.module("myApp", []);
 
 app.directive('dumbPassword', function(){
+    var validElement = angular.element("<div>{{model.input}}</div>");
+    var link = function(scope) {
+        scope.$watch("model.input", function(value){
+            if (value === 'password') {
+                validElement.toggleClass("alert callout");
+            }
+        })
+    };
     return {
         restrict: 'E',
         replace: true,
-        template: "<div>\n  <input type=\"\" ng-model=\'model.input\'>\n    <div>{{model.input}}</div>\n</div>",
-        link: function(scope) {
+        compile: function (tElement) {
+            tElement.append(validElement);
+            return link;
+        },
+        template: "<div>\n    <input type=\"text\" ng-model=\'model.input\'>\n</div>"
+
+    }
+    /* first version */
+    /*
+    var validElement = angular.element("<div>{{model.input}}</div>");
+    return {
+        restrict: 'E',
+        replace: true,
+        compile: function(tElement) {
+            tElement.append(validElement);
+        },
+        template: "<div>\n    <input type=\"text\" ng-model=\'model.input\'>\n</div>",
+        link: function(scope, element) {
             scope.$watch("model.input", function(value){
                 if (value == 'password') {
+                    element.children(0).toggleClass("alert callout");
                     console.log("change it");
                 }
 
             })
         }
     }
+    */
 });
 
 
