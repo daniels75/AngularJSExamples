4a145bdf79d6419ee3486c89ceac2614ee6e6a28
Daniels <daniel.sadowski75@gmail.com>
2016-01-27 21:25:02 +0100

    angularjs-templateurl - part2

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -1,13 +1,21 @@
 'use strict';
 var app = angular.module("myApp", []);
+// 3rd
+app.run(function ($templateCache) {
+    $templateCache.put("zippy.html",'<div><h3 ng-click="toggleContent()">{{title}}</h3> <div ng-show="isContentVisible" ng-transclude=""></div> </div>');
+    $templateCache.info();
+});
+app.directive("zippy", function($templateCache) {
+    //console.log($templateCache.get("zippy.html"));
 
-app.directive("zippy", function() {
     return {
         restrict: 'E',
         transclude: true,
         scope: {
             title: "@"
         },
+        // 1st templateUrl: 'zippy.html',
+        // 2nd template: $templateCache.get("zippy.html"),
         templateUrl: 'zippy.html',
         link: function(scope) {
             scope.isContentVisible = false;
@@ -15,8 +23,7 @@ app.directive("zippy", function() {
                 scope.isContentVisible = !scope.isContentVisible;
             }
         }
-    }
-})
+    }})
 
 
 
