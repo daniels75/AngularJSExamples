3514e9cdcad5edc34f4df3c57dda82b4db37fa21
Daniels <daniel.sadowski75@gmail.com>
2016-01-27 19:19:17 +0100

    angularjs-templateurl - part1

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -1,19 +1,19 @@
 'use strict';
 var app = angular.module("myApp", []);
 
-app.controller("MyCtrl", function($scope) {
-    console.log($scope);
-});
-app.controller("MyCtrl2", ['$scope', '$http', function(a, b) {
-    console.log(b);
-}]
-);
-app.directive("myDirective", function(){
+app.directive("zippy", function() {
     return {
-        scope: {},
         restrict: 'E',
-        link: function(scope, element, attrs) {
-            console.log(element);
+        transclude: true,
+        scope: {
+            title: "@"
+        },
+        templateUrl: 'zippy.html',
+        link: function(scope) {
+            scope.isContentVisible = false;
+            scope.toggleContent = function(){
+                scope.isContentVisible = !scope.isContentVisible;
+            }
         }
     }
 })
