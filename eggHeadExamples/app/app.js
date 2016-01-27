925d1acdea6b173a9437acd2799b6133d2422a9f
Daniels <daniel.sadowski75@gmail.com>
2016-01-27 21:47:05 +0100

    angularjs-ng-view - part1

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -1,30 +1,17 @@
 'use strict';
-var app = angular.module("myApp", []);
-// 3rd
-app.run(function ($templateCache) {
-    $templateCache.put("zippy.html",'<div><h3 ng-click="toggleContent()">{{title}}</h3> <div ng-show="isContentVisible" ng-transclude=""></div> </div>');
-    $templateCache.info();
+var app = angular.module("myApp", ['ngRoute']);
+app.config(function($routeProvider){
+    $routeProvider.when('/',
+        {
+            templateUrl: "app.html",
+            controller: "AppCtrl"
+        })
 });
-app.directive("zippy", function($templateCache) {
-    //console.log($templateCache.get("zippy.html"));
-
-    return {
-        restrict: 'E',
-        transclude: true,
-        scope: {
-            title: "@"
-        },
-        // 1st templateUrl: 'zippy.html',
-        // 2nd template: $templateCache.get("zippy.html"),
-        templateUrl: 'zippy.html',
-        link: function(scope) {
-            scope.isContentVisible = false;
-            scope.toggleContent = function(){
-                scope.isContentVisible = !scope.isContentVisible;
-            }
-        }
-    }})
-
+app.controller("AppCtrl", function($scope){
+    $scope.model = {
+        message: "This is my app!!!"
+    }
+});
 
 
 
