0b5a8ed214c6f68fbe9eb36ce4d7184ac59daeaa
Daniels <daniel.sadowski75@gmail.com>
2016-01-28 19:15:43 +0100

    angularjs-the-config-function - part2

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -1,13 +1,20 @@
 'use strict';
 var app = angular.module("myApp", ['ngRoute']);
 app.config(function($routeProvider){
+    /*
     $routeProvider.when('/',
         {
             templateUrl: "app.html",
             controller: "AppCtrl"
         })
+        */
 });
-app.controller("AppCtrl", function($scope){
+app.controller("AppCtrl", function($scope, $route){
+    // this doesn't work - it is too late
+    $route.routes["/"] = {
+        templateUrl: "app.html",
+        controller: "AppCtrl"
+    }
     $scope.model = {
         message: "This is my app!!!"
     }
