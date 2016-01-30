5c539fb01459d8814244480a252cb9a11794f2ac
Daniels <daniel.sadowski75@gmail.com>
2016-01-30 15:10:30 +0100

    part1

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -5,34 +5,28 @@ app.config(function($routeProvider){
         .when('/',
             {
                 templateUrl: "app.html",
-                controller: "AppCtrl"
+                controller: "AppCtrl",
+                resolve: {
+                    app : function ($q, $timeout){
+                        var defer = $q.defer();
+                        $timeout(function () {
+                            defer.resolve();
+                        },2000)
+
+
+                        return defer.promise;
+                    }
+                }
+
             })
-        .otherwise(
-            {
-                redirectTo: "/"
-            }
-        )
+
 });
 app.controller("AppCtrl", function($scope, $q){
 
-    var defer = $q.defer();
-    defer.promise
-        .then(function(weapon){
-            alert("I promised I would show up - " + weapon)
-            return "bow"
-        })
-        .then(function(weapon){
-            alert("me to - " + weapon)
-            return "axe"
-        })
-        .then(function(weapon){
-            alert("and I - " + weapon)
 
-        })
-    defer.resolve("sword");
 
     $scope.model = {
-        message: "Address "
+        message: "I'm great "
     }
 });
 
