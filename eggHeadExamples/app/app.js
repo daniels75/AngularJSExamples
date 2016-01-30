73d9ea8825c8f1775b8729d405f2cc60f4543e8a
Daniels <daniel.sadowski75@gmail.com>
2016-01-30 18:49:44 +0100

    part1

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -1,43 +1,51 @@
 'use strict';
 var app = angular.module("myApp", ['ngRoute']);
-app.config(function($routeProvider){
-    $routeProvider
-        .when('/',
-            {
-                templateUrl: "app.html",
-                controller: "ViewCtrl",
-                resolve: {
-                    loadData : appCtrl.loadData
-                }
-            })
-});
-app.directive("error", function($rootScope){
+// 1st approach
+/*
+ app.factory("fruit", function(){
+     return {
+        name: "orange1"
+      }
+ })
+ */
+app.provider("game", function() {
+    var type;
     return {
-        restrict: "E",
-        template: '<div class="callout alert" ng-show="isError">Error!!!</div>',
-        link: function(scope){
-            $rootScope.$on("$routeChangeError", function(event, current, previous, rejection){
-                scope.isError = true;
-            })
+        setType : function(value) {
+            this.type = value;
+         },
+        $get : function(){
+            return {
+                title: this.type + "Craft"
+            }
         }
     }
 })
-var appCtrl  = app.controller("AppCtrl", function($rootScope){
-    $rootScope.$on("$routeChangeError", function(event, current, previous, rejection){
-        console.log("Failure - reason: " + rejection);
-    })
-})
-var  viewCtrl = app.controller("ViewCtrl", function($scope, $q){
-    $scope.model = {
-        message: "I'm great "
-    }
-});
-viewCtrl.loadData = function ($q, $timeout, $route){
-    console.log($route)
-    var defer = $q.defer();
-    $timeout(function () {
-        defer.reject("your network is done");
-    },2000)
-    return defer.promise;
-}
+app.config(function($provide, gameProvider){
+    gameProvider.setType("War");
 
+    // 2nd approach
+    $provide.factory("fruit", function() {
+        return {
+            name: "orange2"
+        }
+    })
+    // 2nd approach
+    /*
+    $provide.provider("game1", function(){
+        return {
+            $get: function() {
+                return {
+                    title: "StarCraft2"
+                }
+            }
+
+        }
+    })
+    */
+});
+
+var appCtrl  = app.controller("AppCtrl", function($scope, fruit, game){
+    $scope.fruit = fruit.name;
+    $scope.title = game.title;
+})
