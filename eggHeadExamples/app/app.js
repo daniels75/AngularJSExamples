fd1d39c5f28b50cbac66fab5e8dc93f4f23a2728
Daniels <daniel.sadowski75@gmail.com>
2016-02-09 23:30:02 +0100

    init commit

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -1,13 +1,10 @@
 'use strict';
 var app = angular.module("app", ['ngRoute']);
 
-// turn of debug
-app.config(function($logProvider){
-    $logProvider.debugEnabled(true);
+app.controller("RoomCtrl", function(){
+    this.openDoor = function(){
+        alert("openDoor");
+    }
+    this.buttonTitle = "Simple Button";
 })
-
-app.run(function($rootScope, $log) {
-    $rootScope.$log = $log;
-})
-
 
