46af4f0315c7eeb0cd8cbbe91a5aab2825b2cad8
Daniels <daniel.sadowski75@gmail.com>
2016-02-09 23:01:01 +0100

    init commit

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -1,28 +1,13 @@
 'use strict';
 var app = angular.module("app", ['ngRoute']);
 
-app.directive('clock', function () {
-    return {
-        restrict: 'E',
-        scope: {
-            timezone: '@'
-        },
-        template: '<div>12:00pm {{timezone}}</div>'
-    }
-});
+// turn of debug
+app.config(function($logProvider){
+    $logProvider.debugEnabled(true);
+})
 
-app.directive('panel', function(){
-    return {
-        restrict: 'E',
-        transclude: true,
-        scope: {
-            title: '@'
-        },
-        template: '<div style="border: 3px solid #000000;">' +
-        '<div class="callout secondary">{{title}}</div>' +
-            '<div ng-transclude></div> ' +
-        '</div>'
-    }
+app.run(function($rootScope, $log) {
+    $rootScope.$log = $log;
 })
 
 
