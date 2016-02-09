6373040f7d1e0c85d9f036855331d683d37a186f
Daniels <daniel.sadowski75@gmail.com>
2016-02-10 00:17:53 +0100

    init commit

--- eggHeadExamples/app/app.js
+++ eggHeadExamples/app/app.js
@@ -1,10 +1,62 @@
 'use strict';
 var app = angular.module("app", ['ngRoute']);
 
-app.controller("RoomCtrl", function(){
-    this.openDoor = function(){
-        alert("openDoor");
+app.directive("country", function(){
+    return {
+        restrict: "E",
+        controller: function(){
+            this.makeAnnouncement = function(message) {
+                console.log("Country says: " + message);
+            }
+        }
     }
-    this.buttonTitle = "Simple Button";
 })
+app.directive("state", function(){
+    return {
+        restrict: "E",
+        require: "^country",
+        link: function(scope, element, attrs, countryCtrl) {
+        },
+        controller: function(){
+            this.makeLaw = function(law) {
+                console.log("State says: " + law);
+            }
+        }
+    }
+})
+// 1st
+/*
+app.directive("city", function(){
+    return {
+        restrict: "E",
+        require: "^country",
+        link: function(scope, element, attrs, countryCtrl) {
+            countryCtrl.makeAnnouncement("This city rocks");
+        }
+    }
+})
+*/
+// 2nd
+/*
+app.directive("city", function(){
+    return {
+        restrict: "E",
+        require: "^state",
+        link: function(scope, element, attrs, stateCtrl) {
+            stateCtrl.makeLaw("Jump higher");
+        }
+    }
+})
+*/
 
+// 3rd
+app.directive("city", function(){
+    return {
+        restrict: "E",
+        require: ["^country","^state"],
+        link: function(scope, element, attrs, ctrls) {
+            ctrls[0].makeAnnouncement("from city");
+            ctrls[1].makeLaw("Jump higher");
+        }
+    }
+})
